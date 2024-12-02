2a9c411305b37b450c38bc8c0fc38a3f0e945701
Emily Ng <ngem3@oocl.com>
2024-12-02 15:21:59 +0800

    feat: add POST api for create new todoItem

--- SpringBoot/src/main/java/com/example/TodoList/controller/TodoController.java
+++ SpringBoot/src/main/java/com/example/TodoList/controller/TodoController.java
@@ -2,18 +2,13 @@ package com.example.TodoList.controller;
 
 import com.example.TodoList.model.Todo;
 import com.example.TodoList.service.TodoService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
-@CrossOrigin(origins = "http://localhost:3000")
-
-
 public class TodoController {
     private final TodoService todoService;
 
@@ -26,5 +21,9 @@ public class TodoController {
         return todoService.findAll();
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
 }
